312e65421c88b44eaefad16e27f58a7f7eddb77c
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2023-12-14 12:54:21 -0300

    Styled-component e Rotas Atualizadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom"
+
+import GlobalStyled from "./styles/GlobalStyled"
+import { Container } from "./styles/GlobalStyled"
+
 function App() {
 
   return (
-    <>
+    <Container>
+      <GlobalStyled />
       <h1>Hello World!</h1>
-    </>
+      <ul>
+        <li>
+          <Link to={'/login'}>Login</Link>
+        </li>
+      </ul>
+    </Container>
   )
 }
 
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,20 @@
+import { Title, Paragrafo } from "./styled"
+
+import GlobalStyled from "../../styles/GlobalStyled"
+import { Container } from "../../styles/GlobalStyled"
+
+export default function Login() {
+
+    return (
+        <Container>
+            <GlobalStyled />
+            <Title>
+                LOGIN,
+                <small>Clique para Logar</small>
+            </Title>
+            <Paragrafo>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum molestias id
+                fuga eos a alias ex? Non, eligendi impedit libero architecto est blanditiis!
+                Placeat modi officiis maxime facilis quas dolor?</Paragrafo>
+        </Container>
+    )
+}
