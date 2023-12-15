1558b508a77ac66aca2f0a3e406f48d706cd605d
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2023-12-15 13:53:16 -0300

    Pagina Alunos criada

--- src/pages/Alunos/index.jsx
+++ src/pages/Alunos/index.jsx
@@ -2,20 +2,38 @@
 import propTypes from 'prop-types'
 import { toast } from 'react-toastify'
 import { Navigate } from 'react-router-dom'
-
 import { Link } from 'react-router-dom'
 
+import { useEffect, useState } from 'react'
+import axios from '../../services/axios'
+import { get } from 'lodash'
+
 import { Title } from "./styled"
-import GlobalStyled from "../../styles/GlobalStyled"
 import { Container } from "../../styles/GlobalStyled"
+import { AlunoContainer, FotoContainer } from './styled'
+import { BsFillImageFill, BsPencilSquare, BsBackspaceFill } from "react-icons/bs";
+
+
 
 export default function Alunos({ isClosed }) {
 
     const isLoggedIn = false
 
+    const [alunos, setAlunos] = useState([])
+
+    useEffect(() => {
+        async function getData() {
+            const response = await axios.get('/alunos')
+            setAlunos(response.data)
+        }
+
+        getData()
+    }, [])
 
     if (isClosed && !isLoggedIn) {
+
         toast.error('Eita porra')
+
         return (
             <Navigate to={'/register'} />
         )
@@ -24,15 +42,30 @@ export default function Alunos({ isClosed }) {
 
         return (
             <Container>
-                <GlobalStyled />
+
                 <Title>
                     Lista de Alunos
                 </Title>
-                <ul>
-                    <li>
-                        <Link to={'/aluno'}>Aluno</Link>
-                    </li>
-                </ul>
+
+                <AlunoContainer>
+                    {alunos.map(aluno => (
+                        <div key={String(aluno.id)}>
+                            <FotoContainer>
+                                {get(aluno, 'Fotos[0].url', false) ? (
+                                    <img src={aluno.Fotos[0].url} alt='Fotos da galeria' />
+                                ) : (
+                                    <BsFillImageFill size={36} />
+                                )}
+                            </FotoContainer>
+                            <span>{aluno.nome}</span>
+                            <span>{aluno.email}</span>
+
+                            <Link to={`/aluno/${aluno.id}/edit`}><BsPencilSquare size={16} /></Link>
+                            <Link to={`/aluno/${aluno.id}/delete`}><BsBackspaceFill size={16} /></Link>
+                        </div>
+                    ))}
+                </AlunoContainer>
+
             </Container>
         )
 
--- src/pages/Alunos/styled.jsx
+++ src/pages/Alunos/styled.jsx
@@ -1,3 +1,26 @@
 import styled from "styled-components";
 
 export const Title = styled.h1``
+
+export const AlunoContainer = styled.div`
+margin-top: 30px;
+
+div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
+}
+
+div + div {
+    border-top: 1px solid #eee;
+}
+`
+
+export const FotoContainer = styled.div`
+img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+}
+`
