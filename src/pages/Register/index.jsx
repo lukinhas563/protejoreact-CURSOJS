278987754eba808f5f7658c484f320fae3cb81a5
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2024-01-05 15:24:35 -0300

    Editar usuario

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { toast } from 'react-toastify'
 import { isEmail } from 'validator'
-import { get } from 'lodash'
-import { useNavigate } from "react-router-dom"
-import axios from '../../services/axios'
+import { useDispatch, useSelector } from "react-redux"
+import * as actions from '../../store/modules/auth/actions'
 
 import { Title, Form } from "./styled"
 import GlobalStyled from "../../styles/GlobalStyled"
@@ -12,15 +11,28 @@ import Loading from "../../components/Loading"
 
 export default function Register() {
 
+    const dispatch = useDispatch()
+
+    const id = useSelector(state => state.auth.user.id)
+    const nomeStored = useSelector(state => state.auth.user.nome)
+    const emailStored = useSelector(state => state.auth.user.email)
+    const isLoading = useSelector(state => state.auth.isLoading)
+
     const [nome, setNome] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [isLoading, setIsLoading] = useState(false)
-    const navigate = useNavigate()
+
+
+    useEffect(() => {
+        if (!id) return
+
+        setNome(nomeStored)
+        setEmail(emailStored)
+
+    }, [emailStored, id, nomeStored])
 
     async function handleSubmit(e) {
 
-        setIsLoading(true)
         e.preventDefault()
         let formErrors = false
 
@@ -34,37 +46,14 @@ export default function Register() {
             toast.error('E-mail inválido')
         }
 
-        if (password.length < 6 || password.length > 50) {
+        if (!id && (password.length < 6 || password.length > 50)) {
             formErrors = true
             toast.error('Senha deve ter entre 6 e 50 caracteres')
         }
 
         if (formErrors) return
 
-        setIsLoading(true)
-
-        try {
-
-            await axios.post('/users/', {
-                nome, password, email
-            })
-
-            toast.success('Cadastrado com sucesso')
-
-            setIsLoading(false)
-
-            navigate('/login')
-
-        } catch (err) {
-
-            const errors = get(err, 'response.data.errors', [])
-
-            errors.map(error => toast.error(error))
-
-            setIsLoading(false)
-
-        }
-
+        dispatch(actions.registerRequest({ nome, email, password, id }))
     }
 
     return (
@@ -72,7 +61,7 @@ export default function Register() {
             <Loading isLoading={isLoading} />
             <GlobalStyled />
             <Title>
-                Crie sua conta
+                {id ? 'Editar dados' : 'Crie sua conta'}
             </Title>
             <Form onSubmit={(e) => handleSubmit(e)}>
                 <label htmlFor="nome">
@@ -88,7 +77,7 @@ export default function Register() {
                     <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Sua senha" />
                 </label>
 
-                <button type="submit">Criar minha conta</button>
+                <button type="submit">{id ? 'Salvar' : 'Criar minha conta'}</button>
             </Form>
         </Container>
     )
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -37,9 +37,65 @@ function persistRehydrate({ payload }) {
     axios.defaults.headers.Authorization = `Bearer ${token}`
 }
 
+function* registerRequest({ payload }) {
+
+    const { id, nome, email, password } = payload
+
+    try {
+
+        if (id) {
+
+            yield call(axios.put, '/users', {
+                email,
+                nome,
+                password: password || undefined,
+            })
+
+            toast.success('Conta alterada com sucesso')
+
+            yield put(actions.registerUpdatedSuccess({ nome, email, password }))
+
+        } else {
+
+            yield call(axios.post, '/users', {
+                email,
+                nome,
+                password
+            })
+
+            toast.success('Conta criada com sucesso')
+
+            yield put(actions.registerCreatedSuccess({ nome, email, password }))
+        }
+
+    } catch (e) {
+
+        const errors = get(e, 'response.data.error', [])
+        const status = get(e, 'response.status', 0)
+
+        if (status === 401) {
+
+            toast.error('Você precisa logar novamente.')
+            yield put(actions.loginFailure())
+            return
+        }
+
+        if (errors.lenght > 0) {
+            errors.map(error => toast.error(error))
+        } else {
+            toast.error('Erro desconhecido')
+        }
+
+        return yield put(actions.registerFailure())
+    }
+
+    return
+}
+
 export default all([
 
     takeLatest(types.LOGIN_REQUEST, loginRequest),
-    takeLatest(types.PERSIST_REHYDRATE, persistRehydrate)
+    takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+    takeLatest(types.REGISTER_REQUEST, registerRequest),
 
 ])
